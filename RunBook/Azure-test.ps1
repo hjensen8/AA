f14cb760dd38994191d51ba0f46be1597886c4e7
hjensen8 <hjensen8@gmail.com>
2018-07-31 13:46:16 +0200

    Created Azure-test.ps1 in AzureAutomation.

---
+++ RunBook/Azure-test.ps1
@@ -0,0 +1,21 @@
+param(
+     [int64]$StartTsUtcTicks
+)
+$VerbosePreference = "Continue"
+$DebugPreference = "Continue"
+$InformationPreference = "Continue"
+$WarningPreference = "Continue"
+$ErrorActionPreference = "Continue"
+
+Write-Verbose "Verbose"
+Write-Debug "Debug"
+Write-Information "Information"
+Write-Warning "Warning"
+Write-Error "Error"
+
+$startTsUtc = ([DateTime]($StartTsUtcTicks))
+$endTsUtc = (Get-Date).ToUniversalTime()
+
+$delay = ([Math]::Round(($endTsUtc -$startTsUtc).TotalSeconds)).ToString()
+((Get-Date).ToString() + " " + $delay + " " )
+
