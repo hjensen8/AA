3e2e7e47e19dd8009a561e3b661a7df6f0d1b492
HTJ <htj@simcorp.com>
2018-08-02 10:22:36 +0200

    .

--- RunBook/MainRunBook.ps1
+++ RunBook/MainRunBook.ps1
@@ -1,6 +1,3 @@
-.\SubRunbook1.ps1 -input1 "Input 1" -input2 "Input 2"
-.\SubRunbook1.ps2 -input1 "Input 3" -input2 "Input 4"
-.\SubRunbook1.ps3 -input1 "Input 5" -input2 "Input 5"
-
-
-
+./SubRunbook1.ps1 -input1 "Input 1" -input2 "Input 2"
+./SubRunbook1.ps1 -input1 "Input 3" -input2 "Input 4"
+./SubRunbook1.ps1 -input1 "Input 5" -input2 "Input 5"
